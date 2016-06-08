01ca90199c07bf68190c0ac2dee9bcf1f3e1a7c0
lenyeon <lenyeon@gmail.com>
2016-06-08 15:11:14 +0900

    blog.shop_detail 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Shop
 
 def index(request):
     shop_list = Shop.objects.all()
     return render(request, 'blog/index.html', {
         'shop_list': shop_list,
+    })
+
+def shop_detail(request, pk):
+    shop = get_object_or_404(Shop, pk=pk)
+    return render(request, 'blog/shop_detail.html', {
+        'shop': shop,
     })
