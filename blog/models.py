d5ea676d7b9433f01b432c4292b91edb7ea5f990
lenyeon <lenyeon@gmail.com>
2016-06-08 14:39:15 +0900

    blog.Shop 모델 / 마이그레이션 적용

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    phonenumber = models.TextField()
+    address = models.TextField()
+    photo = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
