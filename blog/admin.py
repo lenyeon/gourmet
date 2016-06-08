b8faa84db55b14d425a06d70b8063b2fc2742551
lenyeon <lenyeon@gmail.com>
2016-06-08 14:48:25 +0900

    blog.Shop 모델 admin에 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 from django.contrib.humanize.templatetags.humanize import intcomma
 from .models import Shop
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['name', 'content_size', 'created_at']
+
+    def content_size(self, shop):
+        return '%s자' % intcomma(len(shop.content))
+
+admin.site.register(Shop, ShopAdmin)
