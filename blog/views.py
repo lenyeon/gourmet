00ef653529cba1e68ba711b7f2c838878cd9c887
lenyeon <lenyeon@gmail.com>
2016-06-08 16:05:37 +0900

    리뷰 등록 시 post/user 필드를 코드를 통해 등록

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Shop
 from .forms import ReviewForm
@@ -14,11 +15,16 @@ def shop_detail(request, pk):
         'shop': shop,
     })
 
+@login_required
 def review_new(request, shop_pk):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review = form.save()
+            review = form.save(commit=False)
+            review = form.save(commit=False)
+            review.shop = get_object_or_404(Shop, pk=shop_pk)
+            shop.user = request.user
+            shop.save()
             return redirect('blog:shop_detail', shop_pk)
     else:
         form = ReviewForm()
