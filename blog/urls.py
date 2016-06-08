4dafc8721b3dede60c0116afe5c51623c9cc3315
lenyeon <lenyeon@gmail.com>
2016-06-08 16:11:48 +0900

    댓글 수정 구현

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<pk>\d+)/$', views.shop_detail, name='shop_detail'),
     url(r'^(?P<shop_pk>\d+)/reviews/new/$', views.review_new, name='review_new'),
-]
+    url(r'^(?P<shop_pk>\d+)/reviews/(?P<pk>\d+)/edit/$', views.review_edit, name='review_edit'),]
